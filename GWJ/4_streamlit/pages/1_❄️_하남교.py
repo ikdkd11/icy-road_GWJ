4fb6832279857b7f130f4ac061fdef588f5a657e
ikdkd11 <kdkime86@gmail.com>
2024-05-21 15:38:21 +0900

    Update 1_❄️_하남교.py

--- GWJ/4_streamlit/pages/1_❄️_하남교.py
+++ GWJ/4_streamlit/pages/1_❄️_하남교.py
@@ -42,8 +42,8 @@ box11 = plotbox.box1
 col1, col2 = st.columns([1,1])
 with col1:
     option = st.selectbox('표시 이미지 선택:',
-                 ['위험구간2(남해고속도로 교차점 - 옥전교) 위성사진',
-                  '위험구간2(남해고속도로 교차점 - 옥전교) 도로교차지점 거리뷰',
+                 [
+                  '하남교 측면 사진',
                   '지도 시각화(1차 관측)',
                   '지도 시각화(2차 관측)'
                   ])
@@ -51,15 +51,13 @@ image_url2 = (
     "https://github.com/ikdkd11/dashboard/blob/main/python-for-realestate-data-main/0_data/streamlit_data/%EC%98%A5%EC%A0%84%EA%B5%90.png?raw=true"
 )   
 image_url3 = (
-    "https://github.com/ikdkd11/dashboard/blob/main/python-for-realestate-data-main/0_data/streamlit_data/%EA%B5%90%EA%B0%81%ED%95%98%EB%B6%80.png?raw=true"
+    "https://raw.githubusercontent.com/ikdkd11/icy-road_GWJ/main/GWJ/0_data/streamlit_data/%ED%95%98%EB%82%A8%EA%B5%902.png"
 )            
-response = requests.get(image_url2)
+response = requests.get(image_url3)
 image1 = Image.open(BytesIO(response.content))
 with col2:
-    if option == '위험구간2(남해고속도로 교차점 - 옥전교) 위성사진':
-        st.image(image_url2)  # 해당 이미지 파일의 경로
-    elif option == '위험구간2(남해고속도로 교차점 - 옥전교) 도로교차지점 거리뷰':
-        st.image(image_url3)
+    if option == '하남교 측면 사진':
+        st.image(image_url3)  # 해당 이미지 파일의 경로
     elif option == '지도 시각화(1차 관측)':
         col2.plotly_chart(map1_1, height = 1080, use_container_width = True)
     elif option == '지도 시각화(2차 관측)':
@@ -67,13 +65,13 @@ with col2:
         # map1은 사전에 정의한 지도 객체
         # 예: map1 = folium.Map(location=[45.372, -121.6972], zoom_start=12, tiles="Stamen Terrain")
         #st.map(map1)  # 'map1'을 미리 정의하고 해당 객체를 여기에 표시
-st.subheader('위험구간2(남해고속도로 고각하부-옥전교) 1~4차 관측회차 별 시계열 그래프 및 박스그림                                                                               ')
+st.subheader('저온구간1(하남교) 관측회차 별 시계열 그래프 및 박스그림                                                                               ')
 col1, col2 = st.columns([1,1])
 col1.plotly_chart(grph, use_container_width = True)
 with col2:
     tab1, tab2, tab3 = st.tabs(["Table", "Graph1", "Graph2"])
     with tab1:
-       st.subheader('위험구간(옥전교) 중 최저 노면온도 기록구간 진입 전/후 평균 노면온도 비교')
+       st.subheader('하남교 저온구간 및 일반구간 노면온도 비교')
        st.table(table2.head(5))
     with tab2:
          st.plotly_chart(grbp22, use_container_width=True)
