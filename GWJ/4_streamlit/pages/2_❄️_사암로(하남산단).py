5f3e2477c815a8f4e891fca2d097d6f11563da3e
ikdkd11 <kdkime86@gmail.com>
2024-05-21 16:12:54 +0900

    Update 2_❄️_사암로(하남산단).py

--- GWJ/4_streamlit/pages/2_❄️_사암로(하남산단).py
+++ GWJ/4_streamlit/pages/2_❄️_사암로(하남산단).py
@@ -38,12 +38,12 @@ with col1:
                   '지도 시각화(2차 관측)'
                   ])
 image_url = (
-    "https://github.com/ikdkd11/dashboard/blob/main/python-for-realestate-data-main/0_data/streamlit_data/%EB%B2%8C%EA%B5%90%EB%8C%80%EA%B5%90.png?raw=true"
+    "https://raw.githubusercontent.com/ikdkd11/icy-road_GWJ/main/GWJ/0_data/streamlit_data/%EC%82%AC%EC%95%94%EB%A1%9C%20%EC%A0%84%EA%B2%BD.png"
 )               
 response = requests.get(image_url)
 image1 = Image.open(BytesIO(response.content))
 with col2:
-    if option == '위험구간3(벌교대교-장양육교) 위성사진':
+    if option == '저온구간2(사암로) 정면 사진':
         st.image(image_url)  # 해당 이미지 파일의 경로
     elif option == '지도 시각화(1차 관측)':
         col2.plotly_chart(map2_1, height = 1080, use_container_width = True)
@@ -53,14 +53,14 @@ with col2:
         # 예: map1 = folium.Map(location=[45.372, -121.6972], zoom_start=12, tiles="Stamen Terrain")
         #st.map(map1)  # 'map1'을 미리 정의하고 해당 객체를 여기에 표시
 
-st.subheader('위험구간3(벌교대교-장양육교) 1~4차 관측회차 별 시계열 그래프 및 박스그림                                                                               ')        
+st.subheader('저온구간2(사암로) 관측회차 별 시계열 그래프 및 박스그림')        
 col1, col2 = st.columns([1,1])
 col1.plotly_chart(grph2, use_container_width = True)
 
 with col2:
     tab1, tab2, tab3 = st.tabs(["Table", "Graph1", "Graph2"])
     with tab1:
-       st.subheader('위험구간(벌교대교) 중 최저 노면온도 기록구간 별 노면온도 비교')
+       st.subheader('사암로 저온구간 및 일반구간 노면온도 비교')
        st.table(table2.head(5))
     with tab2:
          st.plotly_chart(table22, use_container_width=True)
