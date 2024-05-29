c6b27777123dd557ab7737667ace20b7b99d367d
ikdkd11 <kdkime86@gmail.com>
2024-05-29 17:16:03 +0900

    Update 메인화면_.py

--- GWJ/4_streamlit/메인화면_.py
+++ GWJ/4_streamlit/메인화면_.py
@@ -37,7 +37,7 @@ else:
     header_text = """
 <h2>
 <구간B> 광주광역시 일곡사거리(일곡지구)~조선대입구(동구청)<br>
-결빙관측 관측회차 별 분석정보
+       결빙관측 관측회차 별 분석정보
 </h2>
 """
     st.markdown(header_text, unsafe_allow_html=True)
