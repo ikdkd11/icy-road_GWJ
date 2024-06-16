79b88c2daf57e4c40e2edd5bbb076e671bea2ba3
ikdkd11 <kdkime86@gmail.com>
2024-06-16 13:13:39 +0900

    Update 1_❄️_첫번째 저온구간.py

--- GWJ/4_streamlit/pages/1_❄️_첫번째 저온구간.py
+++ GWJ/4_streamlit/pages/1_❄️_첫번째 저온구간.py
@@ -113,12 +113,12 @@ else:
         "https://github.com/ikdkd11/dashboard/blob/main/python-for-realestate-data-main/0_data/streamlit_data/%EC%98%A5%EC%A0%84%EA%B5%90.png?raw=true"
     )   
     image_url3 = (
-        "https://raw.githubusercontent.com/ikdkd11/icy-road_GWJ/main/GWJ/0_data/streamlit_data/%ED%95%98%EB%82%A8%EA%B5%902.png"
+        "https://raw.githubusercontent.com/ikdkd11/icy-road_GWJ/main/GWJ/0_data/bukgu/%EC%82%AC%EC%A7%841.png"
     )            
     response = requests.get(image_url3)
     image1 = Image.open(BytesIO(response.content))
     with col2:
-        if option == '하남교 측면 사진':
+        if option == '최저 노면온도 관측구간 전경':
             st.image(image_url3)  # 해당 이미지 파일의 경로
         elif option == '지도 시각화(1차 관측)':
             col2.plotly_chart(bmap1_1, height = 1080, use_container_width = True)
@@ -127,13 +127,13 @@ else:
             # map1은 사전에 정의한 지도 객체
             # 예: map1 = folium.Map(location=[45.372, -121.6972], zoom_start=12, tiles="Stamen Terrain")
             #st.map(map1)  # 'map1'을 미리 정의하고 해당 객체를 여기에 표시
-        st.subheader('저온구간1(하남교) 관측회차 별 시계열 그래프 및 박스그림                                                                               ')
+        st.subheader('구간B)첫번째 저온구간- 국립광주박물관(서광주IC) 관측회차 별 시계열 그래프 및 박스그림                                                                               ')
     col1, col2 = st.columns([1,1])
     col1.plotly_chart(grph, use_container_width = True)
     with col2:
         tab1, tab2, tab3 = st.tabs(["Table", "Graph1", "Graph2"])
         with tab1:
-            st.subheader('하남교 저온구간 및 일반구간 노면온도 비교')
+            st.subheader('저온구간 및 일반구간 노면온도 비교')
         st.table(table2.head(5))
         with tab2:
             st.plotly_chart(grbp22, use_container_width=True)
